82070a5fa494cd9ae84e876c5729f60404faef65
Tagura <tagura1903@gmail.com>
2026-03-29 15:53:39 +0300

    added sorting function from mylib

--- 12/Task/main.c
+++ 12/Task/main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#include <mylib/mylib.h>
+// #include <mylib/mylib.h>
 
 
 enum {name_length=50, max_routes=20};
@@ -11,6 +11,20 @@ struct MARSH {
     char TERM[name_length];
     unsigned short NUMBER;
 };
+
+
+void sort(int *arr, int n) {
+    int i, j, t;
+    for (i=0; i<n-1; i++) {
+        for (j=0; j<n-i-1; j++) {
+            if (arr[j+1] < arr[j]) {
+                t = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = t;
+            }
+        }
+    }
+}
 
 
 int get_index_route(struct MARSH *routes, short cnt, short route_number) {
