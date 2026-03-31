5b9441135ce8dd2e55b5c7b3b59ab3df9ef86df3
Tagura <tagura1903@gmail.com>
2026-03-31 22:27:29 +0300

    Added the abillity to delete routes

--- 12/Task/main.c
+++ 12/Task/main.c
@@ -59,12 +59,10 @@ int add_route(char FNAME[], struct MARSH *routes, short *cnt) {
 
     FILE *fp;
     if ((fp = fopen(FNAME, "ab")) == NULL) {perror(FNAME); return 1;}
-    fwrite(cnt, sizeof(*cnt), 1, fp);
-    fwrite(&routes[*cnt], sizeof(struct MARSH), 1, fp);
-    (*cnt)++;
+    fwrite(&routes[(*cnt)++], sizeof(struct MARSH), 1, fp);
+    fclose(fp);
 
     printf("Маршрут успешно добавлен.\n");
-    fclose(fp);
     return 0;
 }
 
@@ -81,14 +79,17 @@ int *get_available_routes(struct MARSH *routes, short cnt) {
 
 
 int read_to_struct_from_file(char FNAME[], struct MARSH *routes, short *cnt) {
+    *cnt = 0;  // обнуляем счётчик перед чтением
     FILE *fp;
     if ((fp = fopen(FNAME, "rb")) == NULL) {
-        perror(FNAME);
-        return 1;
+        // если файла нет -- не ошибка, просто нет маршрутов
+        return 0;
     }
 
-    fread(cnt, sizeof(*cnt), 1, fp);
-    fread(routes, sizeof(struct MARSH), max_routes, fp);
+    // читаем структуры, пока есть данные и не превышен max_routes
+    while (*cnt < max_routes && fread(&routes[*cnt], sizeof(struct MARSH), 1, fp) == 1) {
+        (*cnt)++;
+    }
 
     fclose(fp);
     return 0;
@@ -120,45 +121,87 @@ void get_route_info(struct MARSH *routes, short cnt) {
     } else {
         printf("Маршрут не найден.");
     }
+
+    free(availables);  // освобождаем память
 }
 
- 
-int main() {
-    char *FNAME = "structures.txt";
-    short routes_cnt;
-    struct MARSH *TRAFIC = (struct MARSH*)malloc(max_routes * sizeof(struct MARSH));
 
-    read_to_struct_from_file(FNAME, TRAFIC, &routes_cnt); // загружаем (читаем) данные из файла в структурный массив TRAFIC
+int write_main_routes(char FNAME[]) {
+    struct MARSH TRAFIC[8] = {
+        {"Юбилейный", "Ж/Д вокзал", 2},
+        {"Ж/Д вокзал", "Аэропорт Волгоград", 6},
+        {"Инструментальный завод", "Ж/Д вокзал", 21},
+        {"Мясникова", "ТРЦ «Акварель»", 25},
+        {"Больничный комплекс", "Речной вокзал", 35},
+        {"ДК «Строитель»", "Детский центр", 55},
+        {"Грамши", "Технологический колледж", 59},
+        {"Пл. Куйбышева", "ЦСМ", 65}
+    };
+
+    FILE *fp;
+    if ((fp = fopen(FNAME, "wb")) == NULL) {perror(FNAME); return 0;}
+    fwrite(TRAFIC, sizeof(struct MARSH), 8, fp);
+    return 1;
+}
+
+
+int remove_route(char FNAME[], struct MARSH *routes, short *cnt) {
+    short selected_route;
+
+    printf("\nВведите номер маршрута: ");
+    scanf("%hd", &selected_route);
+
+    int index_route = get_index_route(routes, *cnt, selected_route);
+
+    // сдвигаем массив структур на 1 элемент влево, начиная с индекса этого маршрута
+    for (int i=index_route; i<(*cnt-1); i++) {
+        routes[i] = routes[i+1];
+    }
+    (*cnt)--;
+
+    FILE *fp;
+    if ((fp = fopen(FNAME, "rb+")) == NULL) {perror(FNAME); return 1;}
+    fwrite(routes, sizeof(struct MARSH), *cnt, fp);
+    fclose(fp);
+
+    printf("Маршрут удалён\n");
+    return 1;
+}
+
+
+void run_program(char FNAME[], struct MARSH *routes, short *cnt) {
+    read_to_struct_from_file(FNAME, routes, cnt); // загружаем (читаем) данные из файла в структурный массив TRAFIC
 
     while (1) {
         short action;
-        printf("\nВыберете действие (1/2): \n\
-\t1. Добавить маршрут\n\
-\t2. Получить информацию о маршруте\n");
+        printf("\nВыберете действие (1/2/3): \n\
+\t1. Получить информацию о маршруте\n\
+\t2. Добавить маршрут\n\
+\t3. Удалить маршрут.\n");
+        printf("Введите действие: ");
         scanf("%hd", &action);
+        while (getchar() != '\n'); // очищаем буфер после scanf
         
-        if (action == 1) {
-            add_route(FNAME, TRAFIC, &routes_cnt);
-        } else if (action == 2) {
-            get_route_info(TRAFIC, routes_cnt);
+        switch (action) {
+            case 1: get_route_info(routes, *cnt); break;
+            case 2: add_route(FNAME, routes, cnt); break;
+            case 3: remove_route(FNAME, routes, cnt); break;
         }
 
         printf("\nНажмите Enter для продолжения/Ctrl+C для завершения...");
         while (getchar() != '\n');
         getchar();
     }
-
-    free(TRAFIC);
-    return 0;
 }
 
-// struct MARSH TRAFIC[max_routes] = {
-//         {"Юбилейный", "Ж/Д вокзал", 2},
-//         {"Ж/Д вокзал", "Аэропорт Волгоград", 6},
-//         {"Инструментальный завод", "Ж/Д вокзал", 21},
-//         {"Мясникова", "ТРЦ «Акварель»", 25},
-//         {"Больничный комплекс", "Речной вокзал", 35},
-//         {"ДК «Строитель»", "Детский центр", 55},
-//         {"Грамши", "Технологический колледж", 59},
-//         {"Пл. Куйбышева", "ЦСМ", 65}
-//     };
+ 
+int main() {
+    char *FNAME = "structures.txt";
+    short routes_cnt = 0;   // инициализируем нулём
+    struct MARSH *TRAFIC = (struct MARSH*)malloc(max_routes * sizeof(struct MARSH));
+
+    run_program(FNAME, TRAFIC, &routes_cnt);
+    free(TRAFIC);
+    
+    return 0;
+}
