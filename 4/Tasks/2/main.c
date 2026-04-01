f14f0a255f211ad7ac930abddf5bfe87f6adea3b
Fox570 <tagura1903@gmail.com>
2026-04-01 18:36:04 +0300

    Delete 4 directory

--- 4/Tasks/2/main.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <math.h>
-
-int main () {
-	char p = 0;
-	int q = 10, r = 0;
-	r++;
-	printf("%d\n", (p&q)|r);
-}
--- 4/Tasks/3/main.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <math.h>
-
-int main () {
-	int n = 2;
-	n/=((n--)-(--n));
-	printf("%d\n", n);
-}
